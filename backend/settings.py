7abc0e5981435c1506f009c1ed9d5f3677ad0295
Keren Zhou <u7190539@anu.edu.au>
2023-05-14 19:28:12 +1000

    test

--- backend/settings.py
+++ backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-ps8h3d*lhu-+w7*39vrfrr%8bwtq)^hk-)+!(6wpzb%9p4n%z9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://jasonshop.herokuapp.com']
 
 
 # Application definition
